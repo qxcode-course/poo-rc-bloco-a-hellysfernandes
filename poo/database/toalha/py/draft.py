0829a034a9af336ba5148203a0160b021da53bde
hellysfernandes <115155706+hellysfernandes@users.noreply.github.com>
2025-09-17 14:55:55 +0000

    intro apo

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,25 @@
+class Towel:
+    def __init__(self):
+        self.color: str = ""
+        self.size: str = ""
+        self.wetness: int = 0
+
+hellys = Towel()
+hellys.color = "preta"
+hellys.size = "g"
+
+miguel = Towel()
+miguel.color = "roxo"
+miguel.size = "p"
+
+print(miguel.color)
+
+print("qual a cor da sua toalha")
+color = input()
+tamanho = input()
+
+towel: Towel = Towel()
+towel.color = color
+towel.size = tamanho
+
+print(f"sua toalha é {towel.color} e {towel.size} ")
