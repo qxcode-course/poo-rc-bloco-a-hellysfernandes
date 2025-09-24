1c8876b1ab66f6c0588561e06e71970767d403e3
hellysfernandes <115155706+hellysfernandes@users.noreply.github.com>
2025-09-24 16:21:29 +0000

    inicioda atividade animal

---
+++ poo/database/animal/py/draft.py
@@ -0,0 +1,23 @@
+class Animal:
+    def __init__(self, spacies: str, sound:str):
+        self.spcacies: str = spacies
+        self.age: int = 0
+        self.sound: str = sound
+
+    def __str__(self) -> str:
+        return f"{self.spcacies}, {self.age}, {self.sound}"
+    
+    def ismakeSound(self) -> str:
+        if self.age == 0:
+            return "--"
+        elif self.age == 1:
+            return f"{self.sound}"
+        elif self.age == 2:
+            return f"{self.sound}"
+        elif self.age == 3:
+            return f"{self.sound}"
+        return "RIP"
+    
+    def ageBy(self, increment: int): 
+        if self.age >= 4:
+            print()
